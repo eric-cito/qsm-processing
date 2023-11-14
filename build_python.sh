066c8897ffc2d4074af5f27ad66473d3feb15f18
Lee Reid <lee.reid@ucsf.edu>
2023-11-14 09:15:54 -0800

    Updated build to add hd bet

--- build_python.sh
+++ build_python.sh
@@ -20,11 +20,9 @@ source env/bin/activate
 #python3 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 
-# # HD-BET
-# git clone https://github.com/MIC-DKFZ/HD-BET
-# mv HD-BET HDBET
-# cd HDBET
-# touch __init__.py
-# echo "folder_with_parameter_files = os.path.join('`pwd`', 'hd-bet_params') # Override by Lee" >> HD_BET/paths.py
-# python3.9 -m pip install -e .
-# cd ..
+# HD-BET
+git clone git@git.ucsf.edu:lee-reid/HD-BET-for-python.git HDBET
+cd HDBET
+echo "folder_with_parameter_files = os.path.join('`pwd`', 'hd-bet_params') # Override by Lee" >> HD_BET/paths.py
+python3.9 -m pip install -e .
+cd ..
